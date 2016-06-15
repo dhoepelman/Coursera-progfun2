58709c3f161830b75a9058eb7db37c000f619256
David Hoepelman <david.hoepelman@ordina.nl>
2016-06-15 14:28:37 +0200

    First version of the assignment

--- ex4/src/main/scala/calculator/Calculator.scala
+++ ex4/src/main/scala/calculator/Calculator.scala
@@ -11,11 +11,31 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
-  }
+        val _eval = bindeval(namedExpressions)
+        namedExpressions.mapValues(e => Signal(_eval(e())))
+      }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    bindeval(references)(expr)
+  }
+
+  private def bindeval(references: Map[String, Signal[Expr]]) : Expr => Double = {
+    def eval(path: Set[String])(expr: Expr) : Double = {
+      def _eval = eval(path) _
+      def binop(op : (Double,Double) => Double)(a: Expr, b: Expr) = op(_eval(a),_eval(b))
+
+      expr match {
+        case Literal(v) => v
+        // Detect cyclical reference, if the path already contains the variable we've detected a loop
+        case Ref(name) if path.contains(name) => Double.NaN
+        case Ref(name) => eval(path + name)(getReferenceExpr(name, references))
+        case Plus(a,b) => binop(_ + _)(a,b)
+        case Minus(a,b) => binop(_ - _)(a,b)
+        case Times(a,b) => binop(_ * _)(a,b)
+        case Divide(a,b) => binop(_ / _)(a,b)
+      }
+    }
+    eval(Set.empty)
   }
 
   /** Get the Expr for a referenced variables.
--- ex4/src/main/scala/calculator/Polynomial.scala
+++ ex4/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,32 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    // Δ = b² - 4ac
+
+    Signal({
+      val av = a()
+      val bv = b()
+      val cv = c()
+      bv * bv - 4 * av * cv
+    })
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    // (-b ± √Δ) / 2a
+    Signal({
+      val dv = delta()
+      val av = a()
+      val bv = b()
+      val cv = c()
+
+      val av2 = 2*av
+
+      Math.sqrt(dv) match {
+        case dsqrt if dsqrt.isNaN => Set()
+        case dsqrt if dsqrt == 0 => Set(-bv / av2)
+        case dsqrt => Set((-bv - dsqrt)/av2, (-bv + dsqrt)/av2)
+      }
+    })
   }
 }
